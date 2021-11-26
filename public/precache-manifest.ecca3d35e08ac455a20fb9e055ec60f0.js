5f95bdd9272ae2a35404a42b632b9afff4571784
timtima <tima_omega@outlook.com>
2021-11-27 00:48:16 +0300

    launched

---
+++ public/precache-manifest.ecca3d35e08ac455a20fb9e055ec60f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f628b1e6b06e794dc53804ac967312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f87f851e43ac5551727c",
+    "url": "/static/css/main.41809ab2.chunk.css"
+  },
+  {
+    "revision": "2d82d0a6d239635d5ed3",
+    "url": "/static/js/2.9a6fb6d4.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/static/js/2.9a6fb6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d6a7e888033194cadd",
+    "url": "/static/js/3.e251de29.chunk.js"
+  },
+  {
+    "revision": "f87f851e43ac5551727c",
+    "url": "/static/js/main.64cf761b.chunk.js"
+  },
+  {
+    "revision": "f97dddd09d24d61e498a",
+    "url": "/static/js/runtime-main.c86f3434.js"
+  }
+]);
